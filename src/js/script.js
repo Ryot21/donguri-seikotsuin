2ec32f198bd612255544fba70938065818d8cb66
Ryota Maeda <>
2021-09-30 02:53:24 +0900

    ・グローバルナビ > 表示アニメーション修正 ・通常ナビゲーション > TB,PC時 > 下スクロール時に固定＋上スクロール固定解除

--- src/js/script.js
+++ src/js/script.js
@@ -1,270 +1,100 @@
 
 $(function () {
 
-  const $body = $('body');
-  const $menu = $('#js-menu');
-  const ua = navigator.userAgent.toLowerCase();
+  var $body = $('body');
+  var $menu = $('#js-menu');
 
-  // mainvisual animation first
-  function topFirst() {
-    $('.c-mv-top__1').css({'opacity': '0'});
-    $('.c-mv-top__2').css({'opacity': '0'});
-    $('.c-mv-top__3').css({'opacity': '0'});
-    $('.c-mv-top__4').css({'opacity': '0'});
-    $('.c-mv-top__5').css({'opacity': '0'});
-    // $('.mv-bottom_slogan').css({'opacity': '0'});
-    // $('.mv-bottom_img').css({'opacity': '0'});
+  AOS.init()//Animation On Scroll libraryを使用する宣言
 
-    var d = new $.Deferred;
-    $('.mv-top__1').animate({
-      'opacity': '1',
-    }, 1500, function(){
-      d.resolve();
-    });
-    return d.promise();
+  $(function() {// 宣言
 
-  }
+    anchorLink();
+    // mvtop()//メイン画像について
+    //           setTimeoutAsync(2000).then(topFirst)//2秒後に表示
+    //           setTimeoutAsync(4000).then(topSecond)
+    //           setTimeoutAsync(6000).then(topThird)
+    //           setTimeoutAsync(8000).then(topFourth)
+    //           setTimeoutAsync(10000).then(topFifth);
 
-  // 2枚目
-  function topSecond() {
-
-    var d = new $.Deferred;
-    $('.mv-top__2').animate({
-      'opacity': '1',
-    }, 1500, function(){
-      d.resolve();
-    });
-    return d.promise();
-
-  }
-
-  // 3枚目
-  function topThird() {
-
-    var d = new $.Deferred;
-    $('.c-mv-top__3').animate({
-      'opacity': '1',
-    }, 1500, function(){
-      d.resolve();
-    });
-    return d.promise();
-
-  }
-
-  // 4枚目
-  function topFourth() {
-
-    var d = new $.Deferred;
-    $('.c-mv-top__4').animate({
-      'opacity': '1',
-    }, 1500, function(){
-      d.resolve();
-    });
-    return d.promise();
-
-  }
-
-    // 5枚目
-    function topFourth() {
-
-      var d = new $.Deferred;
-      $('.c-mv-top__5').animate({
-        'opacity': '1',
-      }, 1500, function(){
-        d.resolve();
-      });
-      return d.promise();
-  
-    }
-
-  //モバイルメニュー
-  class MobileMenu {
-    constructor() {
-        this.DOM = {};
-        this.DOM.btn = document.querySelector('.mobile-menu__btn');
-        this.DOM.cover = document.querySelector('.mobile-menu__cover');
-        this.DOM.container = document.querySelector('#global-container');
-        this.eventType = this._getEventType();
-        this._addEvent();
-    }
-
-    _getEventType() {
-        return window.ontouchstart ? 'touchstart' : 'click';
-    }
-
-    _toggle() {
-        this.DOM.container.classList.toggle('menu-open');
-    }
-
-    _addEvent() {
-        this.DOM.btn.addEventListener(this.eventType, this._toggle.bind(this));
-        this.DOM.cover.addEventListener(this.eventType, this._toggle.bind(this));
-    }
-  }
-
-new MobileMenu();
-
-  
+    // slideAnime();
+    // onScroll();
+    navControl();
+    mouseOver();
+});
 
 
-
-  function topFirst() {
-    var d = new $.Deferred;
-    $('.mv-title').animate({
-      'opacity': '1',
-    }, 1500, function () {
-      d.resolve();
-    });
-    return d.promise();
-  }
-
-  function topSecond() {
-    $('.index-navi li').css({
-      opacity: 0
-    }).each(function (i) {
-      $(this).delay(500 * i).animate({
-        opacity: 1
-      }, 1500);
-    });
-  }
-
-  //slick
-  function slickslide() {
-    // $('.js-slider').slick({
-    //   slidesToShow:  1,
-    //   slidesToScroll: 1,
-    //   autoplay: true,
-    //   autoplaySpeed: 2000,
-    // });
-
-    $('.js-slider').slick({
-      dots: true,
-      infinite: true,
-      autoplay: false,
-      autoplaySpeed: 2000,
-      speed: 300,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      dots: true,
-      centerMode: false,
-      responsive: [
-        {
-          breakpoint: 980,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1,
-          }
-        },
-        {
-          breakpoint: 979,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1
-          }
-        },
-        {
-          breakpoint: 780,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
+function openSubMenu() {
+      if ( $(window).width() < 980 ) {
+        $('#js-menu').css('display','none');
+        if($('#js-menuToggle').hasClass('close')) {
+          $('#js-menuToggle').removeClass('close');
+          $body.removeClass('menu-open');
         }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-      ]
-    });
-  }
+      } else {
+        $('#js-menu').css('display','block');
+      }
+}
+$(window).on('resize', function() {openSubMenu();}).trigger('resize');
 
+//global navigation
+function navControl() {
+      var $menuToggle = $('#js-menuToggle');
+      var $bottomMenuToggle = $('#js-menuToggleClose');
+
+      $('#js-menuToggle').on('click', function(e) {
+        e.preventDefault();
+        if ( $menuToggle.hasClass('close') ) {
+          $menu.fadeOut(300);
+        } else {
+          $menu.fadeIn(300);
+        }
+        $menuToggle.toggleClass('close');
+        $body.toggleClass('menu-open');
+      });
+
+      $('#js-menu, #js-menuToggle, #js-menuToggleClose').on( 'click', function(e) {
+        e.stopPropagation();
+      });
+}
 
   // anchor link
   function anchorLink() {
-    $(document).on('click', 'a[href^="#"]', function (e) {
+    $(document).on('click', 'a[href^="#"]', function(e) {
       e.preventDefault();
 
-      var $el = $(this);
+      var $el     = $(this);
       var $target = $($el.attr('href'));
-      var paddingTop = 150;
-      if ($(window).width() < 1000) {
-        paddingTop = 100;
-      }
+      paddingTop = $('#header').outerHeight();
+
       if (!$target[0]) {
         return;
       }
 
       var offset = $target.offset().top;
-      $('html, body').animate({
-        scrollTop: offset - paddingTop
-      });
+      $('html, body').animate({scrollTop: offset - paddingTop});
     });
   }
 
 
-  $('.js-preciencts-slide1').bxSlider({
-    pager: true,
-    pagerCustom: '.slide__pager1'
-  });
-
-  $('.js-preciencts-slide2').bxSlider({
-    pager: true,
-    pagerCustom: '.slide__pager2'
-  });
-  $('.js-preciencts-slide3').bxSlider({
-    pager: true,
-    pagerCustom: '.slide__pager3'
-  });
-  $('.js-preciencts-slide7').bxSlider({
-    pager: true,
-    pagerCustom: '.slide__pager7'
-  });
-  $('.js-preciencts-slide9').bxSlider({
-    pager: true,
-    pagerCustom: '.slide__pager9'
-  });
-  $('.js-preciencts-slide11').bxSlider({
-    pager: true,
-    pagerCustom: '.slide__pager11'
-  });
-
-
-
-  function spChildnav() {
-    var $childBtn = $('.js-childnav-btn');
-    $childBtn.on('click', function (e) {
-      e.preventDefault();
-      // $(this).next('.menu-child').show();
-      if ($('.menu-list li').hasClass('child-open')) {
-        $('.menu-list li').removeClass('child-open');
-        $(this).next().fadeOut(300);
-      } else {
-        $(this).parent('li').addClass('child-open');
-        $(this).next().fadeIn(300);
-      }
+  //トップ画像
+  // mainvisual animation first
+  // promise用のsetTimeout関数
+  function setTimeoutAsync(delay) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(resolve, delay);
     });
   }
-  //global navigation
 
-  function navControl() {
-    var $menuToggle = $('#js-menuToggle');
-    var $bottomMenuToggle = $('#js-menuToggleClose');
 
-    $('#js-menuToggle').on('click', function (e) {
-      e.preventDefault();
-      if ($menuToggle.hasClass('close')) {
-        $menu.fadeOut(300);
-      } else {
-        $menu.fadeIn(300);
-      }
-      $menuToggle.toggleClass('close');
-      $body.toggleClass('menu-open');
-    });
-
-    $('#js-menu, #js-menuToggle, #js-menuToggleClose').on('click', function (e) {
-      e.stopPropagation();
-    });
-
-  }
-
+  //ニュースティッカー
+  $('.c-newsticker_slider').bxSlider({
+      touchEnabled:false,//リンクを有効にするためスライドをマウスでドラッグした際にスライドの切り替えを可能にする機能を無効化
+      mode: 'vertical',
+      controls: false,
+      auto: 'true',
+      pager: false
+  });
+  //モバイルメニュー
   function mouseOver() {
     $('.js-hover').hover(function () {
       $(this).attr('src', $(this).attr('src').replace('_off', '_on'));
@@ -272,86 +102,86 @@ new MobileMenu();
       $(this).attr('src', $(this).attr('src').replace('_on', '_off'));
     });
   }
+});
+
+// ページTOPボタン
+// jQuery(function() {
+//   var pagetop = $('#page_top');   
+//   pagetop.hide();
+//   $(window).scroll(function () {
+//       if ($(this).scrollTop() > 500) {  //100pxスクロールしたら表示
+//           pagetop.fadeIn();
+//       } else {
+//           pagetop.fadeOut();
+//       }
+//   });
+//   $('a[href^="#"]').click(function(){
+//     var time = 500;
+//     var href= $(this).attr("href");
+//     var target = $(href == "#" ? 'html' : href);
+//     var distance = target.offset().top;
+//     $("html, body").animate({scrollTop:distance}, time, "swing");
+//     return false;
+//   });
+// });
+
+// LINE公式アカウントボタン
+// jQuery(function() {
+//   var pagetop = $('#page_top_line');   
+//   pagetop.hide();
+//   $(window).scroll(function () {
+//       if ($(this).scrollTop() > 500) {  //100pxスクロールしたら表示
+//           pagetop.fadeIn();
+//       } else {
+//           pagetop.fadeOut();
+//       }
+//   });
+//   $('a[href^="#"]').click(function(){
+//     var time = 500;
+//     var href= $(this).attr("href");
+//     var target = $(href == "#" ? 'html' : href);
+//     var distance = target.offset().top;
+//     $("html, body").animate({scrollTop:distance}, time, "swing");
+//     return false;
+//   });
+// });
 
 
-  function onScroll() {
-    $('.l-wrapper').waypoint(function (direction) {
-      if (direction === 'down') {
-        $('#js-pagetop').fadeIn();
-        $('.l-header').addClass('l-header--fixed');
-      }
-      if (direction === 'up') {
-        $('#js-pagetop').fadeOut();
-        $('.l-header').removeClass('l-header--fixed');
-      }
-    }, {
-      offset: -80
-    });
-  }
-
-  function openSubMenu() {
-
-    if ($(window).width() < 1000) {
-      $('#js-menu').css('display', 'none');
-      $('.gnav-child').css('display', 'none');
-      if ($('#js-menuToggle').hasClass('close')) {
-        $('#js-menuToggle').removeClass('close');
-        $body.removeClass('menu-open');
-      }
-    } else {
-      $('#js-menu').css('display', 'none');
-      $('.gnav-child').css('display', 'none');
-    }
-  }
-
-  function accordion() {
-    $('.js-accordion-block').hide();
-    $('.js-accordion').on("click", function () {
-      if ($(this).next().is(':visible')) {
-        $(this).next().slideUp();
-        $(this).removeClass('open');
-
-      } else {
-        $('.js-accordion-block').slideUp();
-        $(this).next().slideDown();
-        $('.js-accordion').not($(this)).removeClass('open');
-        $(this).addClass('open');
-      }
-    });
-  }
+//ブログ記事内の【strongタグ】の装飾
+// $(window).on('scroll',function(){
+//   $("strong").each(function(){
+//     var position = $(this).offset().top;
+//     var scroll = $(window).scrollTop();
+//     var windowHeight = $(window).height();
+//     if (scroll > position - windowHeight){
+//       $(this).addClass('isActive');
+//     }
+//   });
+// });
 
 
-  mouseOver();
-  anchorLink();
-  navControl();
-  onScroll();
-  spChildnav();
-  accordion();
-  slickslide();
-  topFirst().then(topSecond);
-  tabs();
-  urlcopy();
+//スクロール途中からヘッダーを出現させるための設定を関数でまとめる
+function FixedAnime() {
+	var elemTop = $('#area-3').offset().top;//#area-3の位置まできたら
+	var scroll = $(window).scrollTop();
+	if(scroll <= 560){//上から20pxスクロールされたら $('#header').addClass('DownMove');//DownMoveというクラス名を除き } else if (scroll >= elemTop){
+			$('#nav').removeClass('UpMove');//#headerについているUpMoveというクラス名を除く
+			$('#nav').addClass('DownMove');//#headerについているDownMoveというクラス名を付与
 
+		}else{
+			if($('#nav').hasClass('DownMove')){//すでに#headerにDownMoveというクラス名がついていたら
+				$('#nav').removeClass('DownMove');//DownMoveというクラス名を除き
+				$('#nav').addClass('UpMove');//UpnMoveというクラス名を付与
+			}
+		}
+}
 
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+	FixedAnime();/* スクロール途中からヘッダーを出現させる関数を呼ぶ*/
+});
 
-
-  // fire when page is fully loaded
-  $(window).on('load', function () {});
-  $(window).on('resize', function () {
-  }).trigger('resize');
-
-
-  //resize
-  var currentWidth = window.innerWidth;
-  window.addEventListener("resize", function () {
-    if (currentWidth == window.innerWidth) {
-      // ウインドウ横幅が変わっていないため処理をキャンセル。
-      return;
-    }
-    // ウインドウ横幅が変わったのでリサイズと見なす。
-    // 横幅を更新
-    currentWidth = window.innerWidth;
-    winSize = window.innerWidth;
-    openSubMenu();
-  });
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+	FixedAnime();/* スクロール途中からヘッダーを出現させる関数を呼ぶ*/
 });
